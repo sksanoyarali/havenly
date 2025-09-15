d0bdad421248bb85a8726cc3536430a86b82f8ef
SK sanoyar ali <asksanoyar33@gmail.com>
2025-09-15 23:56:52 +0530

    link review with listing schema

--- models/listing.js
+++ models/listing.js
@@ -26,6 +26,12 @@ const listingSchema = new Schema({
   country: {
     type: String,
   },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
 })
 
 const Listing = mongoose.model('Listing', listingSchema)
