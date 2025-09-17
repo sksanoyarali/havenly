f9885ce95d29a5f0c1325321ddf805c0b2460c93
SK sanoyar ali <asksanoyar33@gmail.com>
2025-09-17 20:58:16 +0530

    cookie options added

--- index.js
+++ index.js
@@ -37,6 +37,11 @@ const sessionOptions = {
   secret: 'mysupersecret',
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 }
 app.use(session(sessionOptions))
 app.get('/', (req, res) => {
